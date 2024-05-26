9ff5911bfed7e8efae5c9496cd165435dd40e7e6
Henry Hilton <zetronixofficial@gmail.com>
2024-05-26 20:52:50 +0700

    Commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,197 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.animalinput = new System.Windows.Forms.TextBox();
+            this.colorInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bodypartinput = new System.Windows.Forms.TextBox();
+            this.numberinput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Favorite Animal:";
+            // 
+            // animalinput
+            // 
+            this.animalinput.Location = new System.Drawing.Point(140, 10);
+            this.animalinput.Name = "animalinput";
+            this.animalinput.Size = new System.Drawing.Size(161, 22);
+            this.animalinput.TabIndex = 1;
+            // 
+            // colorInput
+            // 
+            this.colorInput.Location = new System.Drawing.Point(140, 38);
+            this.colorInput.Name = "colorInput";
+            this.colorInput.Size = new System.Drawing.Size(161, 22);
+            this.colorInput.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Favorite Color:";
+            // 
+            // bodypartinput
+            // 
+            this.bodypartinput.Location = new System.Drawing.Point(140, 66);
+            this.bodypartinput.Name = "bodypartinput";
+            this.bodypartinput.Size = new System.Drawing.Size(161, 22);
+            this.bodypartinput.TabIndex = 4;
+            // 
+            // numberinput
+            // 
+            this.numberinput.Location = new System.Drawing.Point(140, 94);
+            this.numberinput.Name = "numberinput";
+            this.numberinput.Size = new System.Drawing.Size(161, 22);
+            this.numberinput.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Favorite Body Part:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Favorite Number:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Summarize";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(113, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(210, 122);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Summary 1:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Summary 2:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(99, 152);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(99, 177);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 16);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "label8";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(320, 205);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numberinput);
+            this.Controls.Add(this.bodypartinput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.colorInput);
+            this.Controls.Add(this.animalinput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Favorite Animal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox animalinput;
+        private System.Windows.Forms.TextBox colorInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox bodypartinput;
+        private System.Windows.Forms.TextBox numberinput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,21 @@ namespace Personal_Project_1
         {
             InitializeComponent();
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label7.Text = colorInput.Text + "" + animalinput.Text;
+            label8.Text = "With" + numberinput.Text + "" + bodypartinput.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            animalinput.Clear(); colorInput.Clear(); bodypartinput.Clear(); numberinput.Clear();
+            label7.Text = ""; label8.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
